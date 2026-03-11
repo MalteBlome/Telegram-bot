d8a7c8de9c825178c0ab82a732411e7bfb16b209
MalteBlome <malte.blome@gmx.net>
2026-03-11 18:05:24 +0100

    TeamST3

--- Schnarchtier/schnarchtier.py
+++ Schnarchtier/schnarchtier.py
@@ -12,13 +12,13 @@ TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 
 # Bilder + Captions
 STEPS = [
-    {"file": "Vulkan1.jpg", "caption": "Es grüßt ganz herzlich das Team Schnarchtier"},
-    {"file": "aperol.jpg", "caption": "Caption für Bild 2"},
-    {"file": "eis.jpg", "caption": "Caption für Bild 3"},
-    {"file": "drink.jpg", "caption": "Caption für Bild 4"},
-    {"file": "sonne.jpg", "caption": "Caption für Bild 5"},
-    {"file": "schlafen.jpg", "caption": "Caption für Bild 6"},
-    {"file": "aufwachen.jpg", "caption": "Caption für Bild 7"},
+    {"file": "Vulkan1.jpg", "caption": "Es grüßt ganz herzlich das Team Mäuschen und Schnarchtier. Dieser kleien Schnarchtier_bot führt dich durch schöne Erinnerungen und Beschreibung warum Schnarchtier und Mäuschen so perfekt zusammen passen."},
+    {"file": "aperol.jpg", "caption": "Die beiden können gut gemeinsam spontan sein und wenn das Wetter danach ruft, dann wird mal schnell alles stehen und liegen gelassen und der nächste beste Platz für einen Aperol in der Sonne gesucht. Dabei geht es viel weniger um den Aperol also viel mehr um das gemeinsame genießen und bewusst Zeit für einader nehmen! <3 Es hilft natürlich wenn das Lieblingsgetränk dann das gleiche ist"},
+    {"file": "eis.jpg", "caption": "Das gilt natürlich nicht nur für Aperol sondern auch für ein schönes Eis...und wenn die Sonne aus dem Herzen von Team Mäuschen und Schnarchtier lacht, dann ist es auch egal, dass es vllt gerade regnet, dann kann man trotzdem mit Begeisterung ein Eis vernaschen!"},
+    {"file": "drink.jpg", "caption": "Die beiden funktionieren soooo gut zusammen, dass sie auch die 5 Wochen auf engstem Raum im Urlaub miteinader total schön und harmonisch miteinader verbracht haben. Sodass am Ende der Abschied schwer gefallen ist weil man sich schon an die gemeinsame Zeit gewöhnt hatte und es sehr genossen wurde. Dieser kurze Zwischenstopp in dem Minihäuschen bei Regen ist das perfekte Sinnbild dafür, dass Mäuschen und Schnarchtier auch auf engstem Raum miteinader perfekt funktionieren und deswegen werden sie es auch ohne Probleme in der kleinen Wohnung in der Neckarstraße 23 schaffen ein gemeinsames zuhasue einzurichten."},
+    {"file": "Sonne.jpg", "caption": "Die beiden verbindet die Lust nach Abendteuern, auch wenn es auf dem Weg dahin, manchmal etwas stürmisch ist, sei es beim Kapf gegen die Höhenmeter des Vulkans oder die Ungewissheit beim erobern einer Höhle, so ist am Ende nur eins wichtig. Gemeinsam neue Erfahrungen und Erinnerungen sammeln, denn wenn sie diese Dinge teilen dann werden sie besonders. Immer zusammen :) "},
+    {"file": "schlafen.jpg", "caption": "Und nur da wo Geborgenheit, Liebe und Zuneigung besteht, da kann eine so wunderbare tiefe und innige Bindung entstehen. Ein Bindung die die Nächte schöner macht und das Einschlafen in einen entspannten Tagesabschluss verwandelt "},
+    {"file": "aufwachen.jpg", "caption": "Wer gemeinsam einschläft, der wacht auch gerne gemeinsam auf. Und wenn das erste am Tag das Lächeln des Partners ist, dann weiß man, dass man auf dem richtigen Weg ist und dann das gibt einem Gewissheit und Sicherheit die richtige Person gefunden zu haben. <3 Vamos: Wir zusammen, niemals allein"},
 ]
 
 
